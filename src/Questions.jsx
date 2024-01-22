3343dee729d3d3098371f2f6a0570eb6cab6b96b
gowtham1817 <gowthamronaldo1711@gmail.com>
2024-01-22 17:06:32 +0530

    first commit

---
+++ src/Questions.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SingleQuestions from './SingleQuestions'
+
+const Questions = ({questions,activeId,toggleQuestion}) => {
+  return (
+      <section className='container'>
+          <h1>QUESTIONS</h1>
+          {questions.map((question)=>{
+              return  <SingleQuestions key={question.id} {...question} activeId={activeId} toggleQuestion={toggleQuestion}/>
+          })}
+      </section>
+  )
+}
+
+export default Questions
